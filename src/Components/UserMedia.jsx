65a89655033c1ac575d26040b2677ad7e24f9b0c
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-11 07:41:53 +0300

    Users next to media

--- src/Components/UserMedia.jsx
+++ src/Components/UserMedia.jsx
@@ -30,15 +30,18 @@ function UserMedia() {
 				let totalUserWhoScored = 0;
 				let totalScore = 0;
 				let repeat = 0;
+				const users = []
 
 				for (const user of userTable()) {
 					const userKey = "Completed" + user.name;
-					if (userKey in mediaInfo[mediaKey].userScores) {
+					const media = mediaInfo[mediaKey];
+					if (userKey in media.userScores) {
 						totalUserCount++;
-						repeat += mediaInfo[mediaKey].userRepeats[userKey];
+						repeat += media.userRepeats[userKey];
 
-						if (mediaInfo[mediaKey].userScores[userKey] > 0) {
-							totalScore += mediaInfo[mediaKey].userScores[userKey];
+						if (media.userScores[userKey] > 0) {
+							totalScore += media.userScores[userKey];
+							users.push({ name: user.name, avatar: user.avatar.medium, score: media.userScores[userKey], repeat: media.userRepeats[userKey] })
 							totalUserWhoScored++;
 						}
 					}
@@ -60,6 +63,7 @@ function UserMedia() {
 					score,
 					repeat,
 					percentage: (totalUserCount / userTable().length),
+					users
 				});
 			});
 		}
@@ -80,17 +84,33 @@ function UserMedia() {
 						{media.banner ? <img className={style.banner} loading="lazy" src={media.banner} /> : <div className={style.banner} style={"background-color: " + media.color}></div>}
 						<div className={style.coverContainer}>
 							<img className={style.cover} loading="lazy" src={media.coverImage} />
-							{media.repeat ? <p className={style.repeat}>{media.repeat}<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z" /></svg></p> : null}
+							{media.repeat && (<p className={style.repeat}>{media.repeat}<RepeatSvg /></p>)}
 							<p className={style.episodes}>{media.episodes}</p>
 							<p className={style.score}>{media.score}</p>
 						</div>
 
-						<p className={style.title}>{media.english}</p>
+						<div className={style.rightContainer}>
+							<p className={style.title}>{media.english}</p>
+							<div className={style.users}>
+								<For each={media.users}>{user => (
+									<div className={style.row}>
+										<img className={style.avatar} src={user.avatar} />
+										<span className={style.name}>{user.name}</span>
+										{user.repeat && (<span className={style.repeat}>{user.repeat}<RepeatSvg /></span>)}
+										<span className={style.score}>{user.score.toFixed(1)}</span>
+									</div>
+								)}</For>
+							</div>
+						</div>
 					</div>
 				</Show>
 			)}</For>
 		</main>
 	)
+}
+
+function RepeatSvg() {
+	return <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z" /></svg>
 }
 
 export default UserMedia;
