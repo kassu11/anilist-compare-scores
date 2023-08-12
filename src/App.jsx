d29382715489688a413ce9e1d90d079945af7e1d
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-12 14:01:09 +0300

    checkbox signals

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import UserSearch from "./Components/UserSearch";
 import UserTable from "./Components/UserTable";
 import UserMedia from "./Components/UserMedia";
 import MediaTypeButtons from "./Components/MediaTypeButtons";
+import ListTypes from "./Components/ListTypes";
 
 function App() {
   return (
@@ -10,17 +11,7 @@ function App() {
         <UserSearch />
         <UserTable />
 
-        <div id="checkboxRow">
-          <ul>
-            <li><input type="checkbox" name="status" id="Completed" checked /> <label htmlFor="Completed">Completed</label></li>
-            <li><input type="checkbox" name="status" id="Planning" checked /> <label htmlFor="Planning">Planning</label></li>
-            <li><input type="checkbox" name="status" id="Watching" checked /> <label htmlFor="Watching">Watching</label></li>
-            <li><input type="checkbox" name="status" id="Rewatched" checked /> <label htmlFor="Rewatched">Rewatched</label></li>
-            <li><input type="checkbox" name="status" id="Paused" checked /> <label htmlFor="Paused">Paused</label></li>
-            <li><input type="checkbox" name="status" id="Dropped" checked /> <label htmlFor="Dropped">Dropped</label></li>
-            <li><input type="checkbox" name="status" id="Custom" checked /> <label htmlFor="Custom">Custom</label></li>
-          </ul>
-        </div>
+        <ListTypes />
 
         <MediaTypeButtons />
       </footer>
---
+++ src/Components/ListTypes.jsx
@@ -0,0 +1,27 @@
+import { createSignal } from "solid-js";
+
+export const [listType, setListType] = createSignal([]);
+
+function ListTypes() {
+	return (
+		<form id="checkboxRow" onInput={e => updateListType(e.currentTarget)} use: updateListType>
+			<ul>
+				<li><input type="checkbox" name="Completed" id="Completed" checked /> <label htmlFor="Completed">Completed</label></li>
+				<li><input type="checkbox" name="Watching" id="Watching" checked /> <label htmlFor="Watching">Watching</label></li>
+				<li><input type="checkbox" name="Rewatched" id="Rewatched" /> <label htmlFor="Rewatched">Rewatched</label></li>
+				<li><input type="checkbox" name="Paused" id="Paused" /> <label htmlFor="Paused">Paused</label></li>
+				<li><input type="checkbox" name="Dropped" id="Dropped" /> <label htmlFor="Dropped">Dropped</label></li>
+				<li><input type="checkbox" name="Planning" id="Planning" /> <label htmlFor="Planning">Planning</label></li>
+				<li><input type="checkbox" name="Custom" id="Custom" /> <label htmlFor="Custom">Custom</label></li>
+			</ul>
+		</form>
+	)
+}
+
+function updateListType(formElem) {
+	const data = Object.fromEntries(new FormData(formElem))
+	const keys = Object.keys(data);
+	setListType(keys);
+}
+
+export default ListTypes;
