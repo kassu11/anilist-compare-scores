edd963db892aeae7c6e7417bf4fd0c2c5dd117f4
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-08-15 19:25:24 +0300

    User table styling

--- src/Components/UserTable.jsx
+++ src/Components/UserTable.jsx
@@ -10,37 +10,59 @@ function UserTable() {
 		<table>
 			<thead>
 				<tr>
-					<th>User</th>
+					<th>Selection</th>
+					<th>Name</th>
 					<th>Anime</th>
-					<th>Mean score</th>
-					<th>Hours watched</th>
+					<th className={style.close}>Score</th>
+					<th className={style.paddingRight}>Hours</th>
 					<th>Manga</th>
-					<th>Mean score</th>
-					<th>Chapters read</th>
+					<th className={style.close}>Score</th>
+					<th className={style.paddingRight}>Chapters</th>
 					<th>Exclude</th>
 				</tr>
 			</thead>
 			<tbody>
 				<For each={userTable()}>{user => (
 					<tr>
-						<td className={style.userCell}>
-							<input type="checkbox" for="enabled" onInput={e => changeUserState(e, user)} checked onClick={multiSelect} />
-							<div className={style.hamburger}></div>
-							<TrashCan user={user} />
-							<img src={user.avatar.medium} alt={user.name} height="25" /> {user.name}</td>
+						<td>
+							<div className={style.center}>
+								<input type="checkbox" for="enabled" onInput={e => changeUserState(e, user)} checked onClick={multiSelect} />
+								<div className={style.hamburger}></div>
+								<TrashCan user={user} />
+							</div>
+						</td>
+						<td>
+							<div className={style.center}>
+								<img src={user.avatar.medium} alt={user.name} height="25" /> {user.name}
+							</div>
+						</td>
 						<td>{user.statistics.anime.count}</td>
-						<td>{user.statistics.anime.meanScore}</td>
+						<td className={style.close}>{user.statistics.anime.meanScore}</td>
 						<td>{Math.round(user.statistics.anime.minutesWatched / 60)}</td>
 						<td>{user.statistics.manga.count}</td>
-						<td>{user.statistics.manga.meanScore}</td>
+						<td className={style.close}>{user.statistics.manga.meanScore}</td>
 						<td>{Math.round(user.statistics.manga.chaptersRead)}</td>
-						<td><input type="checkbox" for="exclude" onInput={e => changeExcludeState(e, user)} onClick={multiSelect} /></td>
+						<td className={style.exclude}><input type="checkbox" for="exclude" onInput={e => changeExcludeState(e, user)} onClick={multiSelect} /></td>
 					</tr>
 				)}</For>
 			</tbody>
 		</table>
 	)
 }
+
+
+function TrashCan({ user }) {
+	return (
+		<div className={style.trash} onClick={() => {
+			const users = userTable().filter((u) => u.id !== user.id);
+			setUserTable(users);
+			updateMediaData(users);
+		}}>
+			<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" /></svg>
+		</div>
+	)
+}
+
 
 function multiSelect(event) {
 	if (!event.shiftKey) return;
@@ -72,20 +94,11 @@ function changeExcludeState(e, user) {
 	updateMediaData(users);
 }
 
-function TrashCan({ user }) {
-	return (
-		<div onClick={() => {
-			const users = userTable().filter((u) => u.id !== user.id);
-			setUserTable(users);
-			updateMediaData(users);
-		}}>
-			<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" /></svg>
-		</div>
-	)
-}
+
 
 function changeUserState(e, user) {
 	user.enabled = e.target.checked;
+	e.target.closest("tr").classList.toggle(style.disabled);
 	const users = [...userTable()];
 	setUserTable(users);
 	updateMediaData(users);
