01e8c44c34aa4dabe18e0c17a4ec1771ba13a612
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-09-15 09:26:58 +0300

    Flavor text

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,11 @@ function App() {
 		<>
 			<footer class="settings">
 				<h1>Anime Score Compare</h1>
+				<p>
+					The interface works, but is not decorated yet! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione atque
+					perferendis facilis eius iste, possimus dicta neque libero, quas sequi minus dolore. Eius dolor cumque, voluptate veritatis
+					maiores rerum.
+				</p>
 				{/* <UserSearch /> */}
 				<UserTable />
 
