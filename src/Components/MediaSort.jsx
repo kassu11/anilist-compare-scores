4ff410b436ee6d55b8536cd00ae623a63588221b
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-10-02 19:54:14 +0300

    Avarage Score

--- src/Components/MediaSort.jsx
+++ src/Components/MediaSort.jsx
@@ -4,9 +4,10 @@ import { updateMediaData } from "./UserMedia";
 function MediaSort() {
 	return (
 		<select id="sort" onInput={sortSelect}>
-			<option value="score">Score</option>
-			<option value="repeat">Repeat</option>
+			<option value="score">User score</option>
+			<option value="repeat">Repeats</option>
 			<option value="title">Title</option>
+			<option value="averageScore">Global Score</option>
 		</select>
 	);
 }
