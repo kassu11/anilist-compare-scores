65bbba77da470c7b3fe54e7df429e45cdfe6d2ee
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-11-06 17:26:35 +0200

    Format api code

--- src/api/anilist.js
+++ src/api/anilist.js
@@ -1,211 +1,213 @@
-const DEBUG = location.hostname === "localhost"
+const DEBUG = location.hostname === "localhost";
 
-const userQuery = `query($page:Int = 1 $id:Int $search:String $sort:[UserSort]=[SEARCH_MATCH]) {
-	Page(page:$page,perPage:20) {
+const userQuery = `query ($page: Int = 1, $id: Int, $search: String, $sort: [UserSort] = [SEARCH_MATCH]) {
+	Page(page: $page, perPage: 20) {
 		pageInfo {
-			total 
-			perPage 
-			currentPage 
-			lastPage 
+			total
+			perPage
+			currentPage
+			lastPage
 			hasNextPage
-		} 
-		users(id:$id search:$search sort:$sort) {
-			id 
-			name 
-      statistics {
-        anime {
-          count
-          meanScore
-          minutesWatched
-        }
-        manga {
-          count
-          meanScore
-          chaptersRead
-        }
-      }
+		}
+		users(id: $id, search: $search, sort: $sort) {
+			id
+			name
+			statistics {
+				anime {
+					count
+					meanScore
+					minutesWatched
+				}
+				manga {
+					count
+					meanScore
+					chaptersRead
+				}
+			}
 			avatar {
-        medium
+				medium
 			}
 		}
 	}
-}`;
+}
+`;
 
 const userVariable = {
-  "page": 1,
-  "type": "USERS",
-  "search": "user_name",
-  "sort": "SEARCH_MATCH"
-}
+	page: 1,
+	type: "USERS",
+	search: "user_name",
+	sort: "SEARCH_MATCH",
+};
 
 const fetchedUsers = {};
 
 export async function fetchUsers(userName) {
-  userName = userName.toLowerCase();
-  if (userName === "") return [];
-  const debugKey = "ani" + userName + "Search";
-  if (fetchedUsers[userName] instanceof Promise) {
-    await fetchedUsers[userName];
-    return fetchedUsers[userName];
-  }
-  if (fetchedUsers[userName]) return fetchedUsers[userName];
+	userName = userName.toLowerCase();
+	if (userName === "") return [];
+	const debugKey = "ani" + userName + "Search";
+	if (fetchedUsers[userName] instanceof Promise) {
+		await fetchedUsers[userName];
+		return fetchedUsers[userName];
+	}
+	if (fetchedUsers[userName]) return fetchedUsers[userName];
 
-  if (DEBUG) {
-    const data = JSON.parse(localStorage.getItem(debugKey));
-    if (data) return fetchedUsers[userName] = data;
-  }
+	if (DEBUG) {
+		const data = JSON.parse(localStorage.getItem(debugKey));
+		if (data) return (fetchedUsers[userName] = data);
+	}
 
-  let fetchingDone;
-  fetchedUsers[userName] = new Promise((resolve) => fetchingDone = resolve);
+	let fetchingDone;
+	fetchedUsers[userName] = new Promise((resolve) => (fetchingDone = resolve));
 
-  const option = optionHandler(userQuery, { ...userVariable, search: userName });
-  const response = await fetch("https://graphql.anilist.co", option);
-  const json = await response.json();
-  fetchedUsers[userName] = json.data.Page.users;
-  if (DEBUG) localStorage.setItem(debugKey, JSON.stringify(fetchedUsers[userName]));
-  fetchingDone();
-  return fetchedUsers[userName];
+	const option = optionHandler(userQuery, { ...userVariable, search: userName });
+	const response = await fetch("https://graphql.anilist.co", option);
+	const json = await response.json();
+	fetchedUsers[userName] = json.data.Page.users;
+	if (DEBUG) localStorage.setItem(debugKey, JSON.stringify(fetchedUsers[userName]));
+	fetchingDone();
+	return fetchedUsers[userName];
 }
 
 export const optionHandler = (query, variables = {}) => ({
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-  body: JSON.stringify({
-    query,
-    variables
-  })
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+	body: JSON.stringify({
+		query,
+		variables,
+	}),
 });
 
 export const fetchedUserMedias = {};
 const userMediaQuery = `query ($userId: Int, $userName: String, $type: MediaType) {
-  MediaListCollection(userId: $userId, userName: $userName, type: $type) {
-    lists {
-      name
-      isCustomList
-      isCompletedList: isSplitCompletedList
-      entries {
-        ...mediaListEntry
-      }
-    }
-    user {
-      id
-      name
-      avatar {
-        large
-      }
-      mediaListOptions {
-        scoreFormat
-        rowOrder
-        animeList {
-          sectionOrder
-          customLists
-          splitCompletedSectionByFormat
-          theme
-        }
-        mangaList {
-          sectionOrder
-          customLists
-          splitCompletedSectionByFormat
-          theme
-        }
-      }
-    }
-  }
+	MediaListCollection(userId: $userId, userName: $userName, type: $type) {
+		lists {
+			name
+			isCustomList
+			isCompletedList: isSplitCompletedList
+			entries {
+				...mediaListEntry
+			}
+		}
+		user {
+			id
+			name
+			avatar {
+				large
+			}
+			mediaListOptions {
+				scoreFormat
+				rowOrder
+				animeList {
+					sectionOrder
+					customLists
+					splitCompletedSectionByFormat
+					theme
+				}
+				mangaList {
+					sectionOrder
+					customLists
+					splitCompletedSectionByFormat
+					theme
+				}
+			}
+		}
+	}
 }
 
 fragment mediaListEntry on MediaList {
-  id
-  mediaId
-  status
-  score(format: POINT_10_DECIMAL)
-  progress
-  progressVolumes
-  repeat
-  priority
-  private
-  hiddenFromStatusLists
-  customLists
-  advancedScores
-  notes
-  updatedAt
-  startedAt {
-    year
-    month
-    day
-  }
-  completedAt {
-    year
-    month
-    day
-  }
-  media {
-    id
-    title {
-      userPreferred
-      romaji
-      english
-      native
-    }
-    coverImage {
-      extraLarge
-      large
-      color
-    }
-    season
-    type
-    format
-    status(version: 2)
-    episodes
-    volumes
-    chapters
-    averageScore
-    popularity
-    isAdult
-    countryOfOrigin
-    genres
-    bannerImage
-    nextAiringEpisode {
-      episode
-    }
-    startDate {
-      year
-      month
-      day
-    }
-  }
-}`
+	id
+	mediaId
+	status
+	score(format: POINT_10_DECIMAL)
+	progress
+	progressVolumes
+	repeat
+	priority
+	private
+	hiddenFromStatusLists
+	customLists
+	advancedScores
+	notes
+	updatedAt
+	startedAt {
+		year
+		month
+		day
+	}
+	completedAt {
+		year
+		month
+		day
+	}
+	media {
+		id
+		title {
+			userPreferred
+			romaji
+			english
+			native
+		}
+		coverImage {
+			extraLarge
+			large
+			color
+		}
+		season
+		type
+		format
+		status(version: 2)
+		episodes
+		volumes
+		chapters
+		averageScore
+		popularity
+		isAdult
+		countryOfOrigin
+		genres
+		bannerImage
+		nextAiringEpisode {
+			episode
+		}
+		startDate {
+			year
+			month
+			day
+		}
+	}
+}
+`;
 
 const userMediaVariable = {
-  "userId": 0,
-  "type": "ANIME"
-}
+	userId: 0,
+	type: "ANIME",
+};
 
 export async function fetchUserMedia({ id, name }, type = "ANIME") {
-  if (name === "") return [];
-  const key = name + type;
-  const debugKey = "ani" + id + type;
-  if (fetchedUserMedias[key] instanceof Promise) {
-    await fetchedUserMedias[key];
-    return fetchedUserMedias[key];
-  }
-  if (fetchedUserMedias[key]) return fetchedUserMedias[key];
+	if (name === "") return [];
+	const key = name + type;
+	const debugKey = "ani" + id + type;
+	if (fetchedUserMedias[key] instanceof Promise) {
+		await fetchedUserMedias[key];
+		return fetchedUserMedias[key];
+	}
+	if (fetchedUserMedias[key]) return fetchedUserMedias[key];
 
-  if (DEBUG) {
-    const data = JSON.parse(localStorage.getItem(debugKey));
-    if (data) return fetchedUserMedias[key] = data;
-  }
+	if (DEBUG) {
+		const data = JSON.parse(localStorage.getItem(debugKey));
+		if (data) return (fetchedUserMedias[key] = data);
+	}
 
-  let fetchingDone;
-  fetchedUserMedias[key] = new Promise((resolve) => fetchingDone = resolve);
+	let fetchingDone;
+	fetchedUserMedias[key] = new Promise((resolve) => (fetchingDone = resolve));
 
-  const option = optionHandler(userMediaQuery, { ...userMediaVariable, userId: id, type });
-  const response = await fetch("https://graphql.anilist.co", option);
-  const json = await response.json();
-  fetchedUserMedias[key] = json.data.MediaListCollection.lists;
-  if (DEBUG) localStorage.setItem(debugKey, JSON.stringify(fetchedUserMedias[key]));
-  fetchingDone();
-  return fetchedUserMedias[key];
+	const option = optionHandler(userMediaQuery, { ...userMediaVariable, userId: id, type });
+	const response = await fetch("https://graphql.anilist.co", option);
+	const json = await response.json();
+	fetchedUserMedias[key] = json.data.MediaListCollection.lists;
+	if (DEBUG) localStorage.setItem(debugKey, JSON.stringify(fetchedUserMedias[key]));
+	fetchingDone();
+	return fetchedUserMedias[key];
 }
